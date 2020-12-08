410ae1a9206e16422b2ac597fd8eab26a0f68ca5
Amanda Ung <amanda17ung@gmail.com>
2020-12-07 16:27:40 -0800

    yay!

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 // JavaScript source code
+const learn = document.querySelector("#learn-section");
+const connect = document.querySelector("#connect-section");
+const create = document.querySelector("#create-section");
+const join = document.querySelector("#join-section");
+const takeMeThere = document.links(href="https://www.reddit.com/r/Embroidery/");
+const startTop = document.querySelector("#top");
+
+function directToReddit() {
+    location.href = "https://www.reddit.com/r/Embroidery/";
+}
+
+function directToTop() {
+    location.href = "#top";
+}
+
+function directToLearn() {
+    location.href = "#learn-section";
+}
+
+function directToConnect() {
+    location.href = "#connect-section";
+}
+
+function directToStart() {
+    location.href = "#create-section";
+}
+
+function directToJoin() {
+    location.href = "#join-section";
+}
+
+learn.addEventListener("click",directToLearn);
+connect.addEventListener("click", directToConnect);
+create.addEventListener("click", directToStart);
+join.addEventListener("click", directToJoin);
+takeMeThere.addEventListener("click", directToReddit);
+startTop.addEventListener("click", directToTop);
+
